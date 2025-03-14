5c5996034f55b0da10a914e9e8b060b329d932f5
Raphael Dos Santos <raphael.rds.work@gmail.com>
2025-03-13 20:27:55 -0400

    Update main.py
    removing personal identifiable values, discord id and aws serv location

--- main.py
+++ main.py
@@ -10,26 +10,7 @@ v1.0.4-Cloud-Beta BIG UPDATE!
 * Uploaded code to aws platform for 24/7 use! 
 * Changed Token auth to integrate with aws ec2 services.
 * Removed import fx for PrettyTables -> TableStyle
-
-* v1.0.4 - Database Edition v2
-* MAJOR: 
-    - Function Added = get_myseedb 
-    - Slash Command = # SLASH COMMAND /MYSEEDBANK
-    - Added Listener Event = startswith 'bert' listener for user interactions
-    - Scoreboard format fixed!
-
-v1.0.3
-* Added function to create initial Phrase.txt that would error on first launch. 
-    -(minor bug exist: Phrase txt file created and default phrase set to: NOT SET. but can not be read on initial start with "NOT SET". user must enter the first phrase with slash command.)
-    -Fixed bug that could not find Phrase.txt file on first use and would error program in terminal. ( refer to the above change )
-
-v1.0.2
-* Added list of premade phrases to randomly respond to 'garden' input from user.
-* Added random import module and created function to retried random phrase from txt file 'GardenPhrases'.
-* Changed Repsone and Format from bot when correct input of the stored keyword in 'Phrase.txt' is given.
-* Added Function to check prize status or create txt file: 
-    Funciton also limits how many times a user claims the prize to 1x by checking if the prize is 'claimed' or 'unclaimed'.
-* Sanitized input to accept anycase from user and convert to lower case.
+see version-history.txt for more info...
 """)
 print(version)
 
@@ -45,7 +26,7 @@ from prettytable import from_db_cursor
 # GRAB VALID TOKEN
 import boto3
 #   AWS SSM Client
-ssm = boto3.client("ssm", region_name="us-east-2")  # Change region if needed
+ssm = boto3.client("ssm", region_name="ctry-region-number")  # Change region if needed
 
 # Retrieve the bot token from Parameter Store
 response = ssm.get_parameter(Name="/discord/bot/token", WithDecryption=True)
@@ -59,7 +40,7 @@ if TOKEN:
 print(f"YOUR LOCATION:",os.getcwd(),'\n')
 
 # SERVER ID / GUILD ID ( CHANGE WHEN DEPLOYING FINAL )
-GUILD_ID = discord.Object(id=1273447123001671721)
+GUILD_ID = discord.Object(id=11111)
 
 # CREATE TABLE DB IF NOT EXIST
 def create_db():
